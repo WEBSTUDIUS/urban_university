537d3b59c76c450784f3b29a3ef5233f7ee72778
Andrey Ilin <andrey.i@fillers.ru>
2024-06-19 01:34:35 +0700

    Base Types

---
+++ main.py
@@ -0,0 +1,25 @@
+someStr = 'Some string'
+print(len(someStr))
+
+first = 10
+second = 15
+summa = first + second
+diff = first - second
+print(summa, diff)
+
+l = [10, 15, 25]
+first = l[0]
+second = l[1]
+third = l[2]
+mean = (first + second + third) / len(l)
+print(mean)
+
+first_string = 'Вторник'
+second_string = 'Понедельник'
+print(second_string + ', ' + first_string)
+
+a = 5
+b = 7
+c = 9
+f = (((a * b) + (a * c)) ** 2) / 2
+print(f)
