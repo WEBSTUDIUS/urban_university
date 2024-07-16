0e5c38c4096f2c3b75027ad9cf82f5bc257e9d15
Andrey Ilin <andrey.i@fillers.ru>
2024-07-17 01:11:23 +0700

    Base Types

---
+++ Module_5/class_attributes.py
@@ -0,0 +1,25 @@
+# Создайте класс House.
+# Вунтри класса House определите метод __init__, в который передадите название и кол-во этажей.
+# Внутри метода __init__ создайте атрибуты объекта self.name и self.number_of_floors, присвойте им переданные значения.
+# Создайте метод go_to с параметром new_floor и напишите логику внутри него на основе описания задачи.
+# Создайте объект класса House с произвольным названием и количеством этажей.
+# Вызовите метод go_to у этого объекта с произвольным числом.
+
+
+class House:
+    def __init__(self, name, floor_numbers):
+        self.name = name
+        self.floor_numbers = floor_numbers
+
+    def go_to(self, new_floor):
+        if new_floor < 0 or new_floor > self.floor_numbers:
+            print('Такого этажа не существует')
+            return
+        for key in range(1, new_floor + 1):
+            print(key)
+
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+h1.go_to(5)
+h2.go_to(10)
