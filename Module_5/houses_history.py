a58e65f5adb58ba51ff18563d2fcd770e20108ce
Andrey Ilin <andrey.i@fillers.ru>
2024-07-18 00:44:28 +0700

    objects

---
+++ Module_5/houses_history.py
@@ -0,0 +1,29 @@
+from operators_reload import House3
+
+
+class House4(House3):
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        return super().__new__(cls)
+
+    def __init__(self, name, floor_numbers):
+        super().__init__(name, floor_numbers)
+        self.houses_history.append(self.name)
+
+    def __del__(self):
+        print(f" {self.name} снесён, но он останется в истории")
+
+
+h1 = House4('ЖК Эльбрус', 10)
+print(House4.houses_history)
+h2 = House4('ЖК Акация', 20)
+print(House4.houses_history)
+h3 = House4('ЖК Матрёшки', 20)
+print(House4.houses_history)
+
+# Удаление объектов
+del h2
+del h3
+
+print(House4.houses_history)
