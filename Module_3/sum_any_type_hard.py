01755859d4e59d05c1c8764a2c35a3b2d8dae45d
Andrey Ilin <andrey.i@fillers.ru>
2024-08-06 00:04:27 +0700

    Module 7

--- Module_3/sum_any_type_hard.py
+++ Module_3/sum_any_type_hard.py
@@ -19,21 +19,35 @@
 
 def calculate_structure_sum(data):
     summa = 0
+
     for i in data:
         if isinstance(i, int) or isinstance(i, float):
             summa += i
-        if isinstance(i, str):
+        elif isinstance(i, str):
             summa += len(i)
-        if isinstance(i, list):
-            for j in i:
-                print(j)
-        if isinstance(i, tuple):
-            for j in i:
-                print(j)
-        if isinstance(i, dict):
-            for j in i:
-                print(j)
-    print('SUMMA IS:', summa)
+        elif isinstance(i, list):
+            summa += calculate_structure_sum(i)
+        elif isinstance(i, tuple):
+            summa += calculate_structure_sum(i)
+        elif isinstance(i, dict):
+            for key, value in i.items():
+                if hasattr(key, '__iter__'):
+                    summa += calculate_structure_sum(key)
+                else:
+                    if isinstance(key, int) or isinstance(key, float):
+                        summa += key
+                    elif isinstance(key, str):
+                        summa += len(key)
+                if hasattr(value, '__iter__'):
+                    summa += calculate_structure_sum(value)
+                else:
+                    if isinstance(value, int) or isinstance(value, float):
+                        summa += value
+                    elif isinstance(value, str):
+                        summa += len(value)
+        elif isinstance(i, set):
+            summa += calculate_structure_sum(i)
+    return summa
 
 
 data_structure = [
@@ -44,9 +58,5 @@ data_structure = [
     ((), [{(2, 'Urban', ('Urban2', 35))}])
 ]
 
-# converted_list = map(str, data_structure)
-# result = ''.join(converted_list)
-# print(result)
 
-print(data_structure[-1])
-calculate_structure_sum(data_structure)
+print(calculate_structure_sum(data_structure))
