3aaa00709f4276193204278d160427263623a320
Andrey Ilin <andrey.i@fillers.ru>
2024-08-06 01:02:54 +0700

    Module 7

--- game.py
+++ game.py
@@ -40,12 +40,36 @@ class Ball(arcade.Sprite):
             self.change_y = -self.change_y
 
 
+class Button:
+    def __init__(self, center_x, center_y, width, height, text, action_function):
+        self.center_x = center_x
+        self.center_y = center_y
+        self.width = width
+        self.height = height
+        self.text = text
+        self.action_function = action_function
+
+    def draw(self):
+        # Draw button
+        arcade.draw_lbwh_rectangle_filled(self.center_x - 100, self.center_y - 25, self.width, self.height, arcade.color.ASH_GREY)
+        # Draw text
+        arcade.draw_text(self.text, self.center_x, self.center_y, arcade.color.BLACK, 18, anchor_x="center", anchor_y="center")
+
+    def is_clicked(self, x, y):
+        return (self.center_x - self.width / 2 < x < self.center_x + self.width / 2 and
+                self.center_y - self.height / 2 < y < self.center_y + self.height / 2)
+
+
 class MyGame(arcade.Window):
     def __init__(self):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+        self.status = None
         arcade.set_background_color(arcade.color.LIGHT_GRAY)
         self.bar = Bar()
         self.ball = Ball()
+        self.score = 0
+        # Create button instance
+        self.button = Button(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2.5, 200, 50, "START AGAIN", self.on_button_click)
         self.all_sprites = arcade.SpriteList()
         self.setup()
 
@@ -61,6 +85,10 @@ class MyGame(arcade.Window):
         self.clear()
         arcade.draw_text("Hello, athletes!", 300, 550, arcade.color.WHITE, 24)
         self.all_sprites.draw()
+        arcade.draw_text(f'Score: {self.score}', 20, SCREEN_HEIGHT - 30, arcade.color.DARK_GREEN, 16)
+        if self.status:
+            arcade.draw_text(self.status, SCREEN_WIDTH / 3.5, SCREEN_HEIGHT / 2, arcade.color.RED, 40)
+            self.button.draw()
 
     def on_update(self, delta):
         self.ball.on_update()
@@ -69,6 +97,10 @@ class MyGame(arcade.Window):
         if arcade.check_for_collision(self.ball, self.bar):
             self.ball.change_y = -self.ball.change_y
             self.ball.bottom = self.bar.top
+            self.score += 1
+        if self.ball.bottom < 0:
+            self.ball.stop()
+            self.status = 'GAME OVER'
 
     def on_key_press(self, symbol: int, modifiers: int):
         if symbol == arcade.key.RIGHT:
@@ -80,6 +112,13 @@ class MyGame(arcade.Window):
     def on_key_release(self, symbol: int, modifiers: int):
         if symbol == arcade.key.RIGHT or symbol == arcade.key.LEFT:
             self.bar.change_x = 0
+
+    def on_mouse_press(self, x, y, button, modifiers):
+        if self.button.is_clicked(x, y):
+            self.button.action_function()
+
+    def on_button_click(self):
+        self.__init__()
 
 
 def main():
