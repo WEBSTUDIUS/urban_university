2c2bec03b6fae8a050e3f0c47eab0a442572ff22
Andrey Ilin <andrey.i@fillers.ru>
2024-08-05 23:44:00 +0700

    Module 7

--- Module_7/file_pos.py
+++ Module_7/file_pos.py
@@ -1,13 +1,13 @@
 def custom_write(file_name, strings):
-    res = {}
+    strings_positions = {}
     file = open(file_name, 'w', encoding='utf-8')
 
     for string in strings:
-        res[(strings.index(string) + 1, file.tell())] = string
+        strings_positions[(strings.index(string) + 1, file.tell())] = string
         file.write(string + '\n')
     file.close()
 
-    return res
+    return strings_positions
 
 
 info = [
