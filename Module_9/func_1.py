6ffea457fcf0928a179faca91b3522ff9c995ab2
Andrey Ilin <andrey.i@fillers.ru>
2024-08-20 10:46:12 +0500

    Module 8

---
+++ Module_9/func_1.py
@@ -0,0 +1,14 @@
+def apply_all_func(int_list, *functions):
+    results = {}
+    try:
+        for function in functions:
+            results[function.__name__] = function(int_list)
+        return results
+    except Exception as e:
+        return e
+
+
+
+print(apply_all_func([6, 20, 15, 9], max, min))
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
+print(apply_all_func(['6', 20, 15, 9], len, sum, sorted))
