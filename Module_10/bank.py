3709069515cf12c1cf899bc96db10013a0e30c55
Andrey Ilin <advernsk@gmail.com>
2024-09-24 11:28:28 +0700

    Module 10

--- Module_10/bank.py
+++ Module_10/bank.py
@@ -2,44 +2,45 @@ from random import randint
 from time import sleep
 from threading import Thread, Lock
 
-class Bank():
+class Bank:
     def __init__(self):
         self.balance = 0
         self.lock = Lock()
 
-    def run(self):
-        ...
-
     def deposit(self):
-        print(self.lock.locked())
         for i in range(100):
             intake = randint(50, 500)
-            if self.balance >= 500 & self.lock.locked():
-                # self.lock.acquire()
+            # Захватываем блокировку перед изменением баланса
+            with self.lock:
                 self.balance += intake
-                print(f"Balance top up: {intake}. Balance: {self.balance}", end='\n')
-                self.lock.release()
-                sleep(0.001)
-
-
+                print(f"Balance top up: {intake}. Balance: {self.balance}")
+            sleep(0.001)
 
     def take(self):
         for i in range(100):
             withdraw = randint(50, 500)
-            print(f"You wanna take: {withdraw}", end='\n')
-            if self.balance > withdraw:
-                self.balance -= withdraw
-                print(f"Withdraw successful: {withdraw}. New balance: {self.balance}", end='\n')
-            else:
-                print(f"Withdraw was declined : {withdraw}. Balance: {self.balance}", end='\n')
-                self.lock.acquire()
+            print(f"You wanna take: {withdraw}")
+            # Захватываем блокировку перед попыткой снять деньги
+            with self.lock:
+                if self.balance >= withdraw:
+                    self.balance -= withdraw
+                    print(f"Withdraw successful: {withdraw}. New balance: {self.balance}")
+                else:
+                    print(f"Withdraw was declined: {withdraw}. Balance: {self.balance}")
+            sleep(0.001)
 
+# Создание объекта банка
 bk = Bank()
-th1 = Thread(target=Bank.deposit, args=(bk,))
-th2 = Thread(target=Bank.take, args=(bk,))
+
+# Запуск потоков для пополнения и снятия средств
+th1 = Thread(target=bk.deposit)
+th2 = Thread(target=bk.take)
+
 th1.start()
 th2.start()
+
 th1.join()
 th2.join()
 
+# Вывод итогового баланса
 print(f'Total balance: {bk.balance}')
--- Module_10/cafe_emulator.py
+++ Module_10/cafe_emulator.py
@@ -1,6 +1,10 @@
+import random
+import time
 from queue import Queue
 from threading import Thread
 from random import randint
+
+from Module_1.strings import example
 
 
 class Table:
@@ -63,3 +67,33 @@ print(cafe.queue.get)
 cafe.guest_arrival(*guests)
 # Обслуживание гостей
 cafe.discuss_guests()
+
+
+# example about cooking =)
+
+class Bulka (Thread) :
+    def __init__(self, queue) :
+        self. queue = queue
+        super().__init__()
+    def run (self):
+        while True:
+            time.sleep (3)
+            if random.random() > 0.5:
+                self.queue.put ('подгорелая булка')
+            else:
+                self.queue.put ( 'нормальная булка')
+
+class Kotleta (Thread) :
+    def __init__(self, queue, count):
+        self.queue = queue
+        self.count = count
+        super().__init__()
+
+    def run (self):
+        while self.count:
+            print(self.queue.qsize())
+            bulka = self.queue.get
+            if bulka == 'нормальная булка' :
+                time. sleep (0.1)
+                self.count -= 1
+            print( 'булок к приготовлению осталось ', self.count)
